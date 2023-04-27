de4019c7ba386ed2f166c2b505bf07658d5bd9a1
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-04-27 18:18:56 -0400

    array_map_method_complete

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(str) {
+  if (!str) {
+    return [
+      "What Does The This Keyword Mean?",
+       "What Is The Constructor OO Pattern?",
+       "Implementing Blockchain Web API",
+       "The Test Driven Development Workflow",
+       "What Is NaN And How Can We Check For It",  "What Is The Difference Between StopPropagation And PreventDefault?",
+       "Immutable State And Pure Functions",
+       "What Is The Difference Between == And ===?",
+       "What Is The Difference Between Event Capturing And Bubbling?",
+      "What Is JSONP?"
+      ]
 }
+  
+  return str.toLowerCase().split('').map(function (word) {
+     return word.charAt(0).toUpperCase().concat(word.substr(1));
+  }).join('');
+}
+
+  
